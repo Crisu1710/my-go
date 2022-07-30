1d58add66383c5ef64276fd188f312b4c7bb5e9d
Robin Schneider <rschneider@vwd.com>
2022-07-30 13:13:09 +0200

    move OpenUrl

---
+++ myHelper/OpenBrowser.go
@@ -0,0 +1,24 @@
+package myHelper
+
+import (
+	"os/exec"
+	"runtime"
+)
+
+// https://stackoverflow.com/questions/39320371/how-start-web-server-to-open-page-in-browser-in-golang
+func OpenUrl(url string) error {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
+}
